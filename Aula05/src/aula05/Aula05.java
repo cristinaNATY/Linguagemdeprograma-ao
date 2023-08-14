238048e417f10eb60770c37d8be491c17f6f8ccd
cristinaNATY <141193140+cristinaNATY@users.noreply.github.com>
2023-08-14 20:25:05 -0300

    Add files via upload

---
+++ Aula05/src/aula05/Aula05.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aula05;
+
+/**
+ *
+ * @author 01979257663
+ */
+public class Aula05 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       int a =10;
+       String b ="10";
+       int c =2;
+       String d = "10";
+       
+       int resultadoA = b + d;
+       int resultadoB = a - c;
+       int resultadoC = a * c;
+       int resultadoD = a / c;
+       int resultadoE = a / c;
+       int resultadoF = a % c;
+       System.out.println (resultadoA);
+       
+    }
+    
+}
