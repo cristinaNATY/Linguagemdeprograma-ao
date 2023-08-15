c949eee0e3f44635df9488bd3a681c9745618949
cristinaNATY <141193140+cristinaNATY@users.noreply.github.com>
2023-08-15 19:57:22 -0300

    Add files via upload

---
+++ aula06.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author 01979257663
+ */
+public class aula06 {
+    public static void main(String[] args) {
+        float nota1 = 8;
+        float nota2 = 5;
+        float nota3 = 7;
+        float nota4 = 8;
+        
+    float media, soma = 0;
+    soma = nota1 + nota2 + nota3 + nota4;
+    media = soma/4;
+    System.out.println("soma:" + soma);
+    System.out.println ("media:" + media);
+    
+    }
+          
+}
