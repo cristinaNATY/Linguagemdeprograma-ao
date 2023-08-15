7b1913060ca27030680b17ddbf89ff729e8cd6dc
cristinaNATY <141193140+cristinaNATY@users.noreply.github.com>
2023-08-15 18:49:26 -0300

    Add files via upload

---
+++ aula04/src/aula04/Aula04.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aula04;
+
+/**
+ *
+ * @author 01979257663
+ */
+public class Aula04 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("o valor do pi é:"+ Math.PI);
+         System.out.println("maximo:"+Math.max(5, 10));
+          System.out.println("minimo:"+ Math.min(5, 10));
+           System.out.println(Math.sqrt(64));
+           
+         System.out.println("Exponencial e potenciaçao\n");
+          System.out.println("o valor de E e:"+ Math.E + "\n\n");
+           System.out.println("'e' elevado ao quadrado =" +Math.exp(2));
+            System.out.println("2 elevado"+ Math.pow(2,3)+"\n\n" );
+        
+    }
+    
+}
