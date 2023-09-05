12a8bfec7d1bb39f766ce64f14eae841c39903f8
cristinaNATY <141193140+cristinaNATY@users.noreply.github.com>
2023-09-05 18:49:41 -0300

    Add files via upload

---
+++ atividade01/src/atividade01/Atividade01.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package atividade01;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author 01979257663
+ */
+public class Atividade01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         Scanner scanner= new Scanner (System.in);
+     int idade;
+     
+     
+     
+     do{
+        System.out.println("Digite sua idade:");
+        escolha = scanner.nextInt();
+        
+    switch (escolha) {
+          case 1:
+        System.out.println("V");
+        break;
+    }
+  
+    }
+    
+}
