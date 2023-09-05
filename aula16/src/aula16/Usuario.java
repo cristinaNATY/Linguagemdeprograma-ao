2953af17c1c5431ea5bcf26975f5a731a0a5c334
cristinaNATY <141193140+cristinaNATY@users.noreply.github.com>
2023-09-05 18:48:24 -0300

    Add files via upload

---
+++ aula16/src/aula16/Usuario.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aula16;
+
+/**
+ *Linguagem de programação
+ * Tela de login
+ * 29/08/2023
+ * @author 01979257663
+ */
+public class Usuario {
+    private String nome = "natalia";
+    private String senha = "5563";
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+   
+    
+    
+    
+}
