a4a57a29fff11c6c11f4399b1a97d1fb19629f47
cristinaNATY <141193140+cristinaNATY@users.noreply.github.com>
2023-09-05 18:48:45 -0300

    Add files via upload

---
+++ aula11/src/aula11/Aula11.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aula11;
+
+/**
+ *
+ * @author 01979257663
+ */
+public class Aula11 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int contador=0;
+        while (contador<10){
+            System.out.println(contador);
+            contador++;
+        }
+        contador=0;
+        for (int i=0; i <10 ; i++){
+            System.out.println(i);
+        }
+    }
+    
+}
